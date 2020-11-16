8025581d772032fd4436d9a000ba2aa0bc7aa356
Sweekriti Satpathy <swsat@microsoft.com>
2020-11-15 16:20:58 -0800

    more things json parser

--- src/nunit.xamarin/Constants.cs
+++ src/nunit.xamarin/Constants.cs
@@ -4,5 +4,7 @@ namespace nunit.xamarin
     public static partial class Constants
     {
         public static string AzureStorageConnectionString = "";
+        public static string DBFrameworkVersionNumber = "";
+        public static string AzureDevOpsBuildNumber = "LOCAL";
     }
 }
