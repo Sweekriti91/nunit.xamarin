9811c460c4713563bd3fa15f642b128702cc09c3
Sweekriti Satpathy <swsat@microsoft.com>
2020-11-16 17:46:59 -0800

    sing test runner stuff

--- src/nunit.xamarin/Constants.cs
+++ src/nunit.xamarin/Constants.cs
@@ -3,8 +3,8 @@ namespace nunit.xamarin
 {
     public static partial class Constants
     {
-        public static string AzureStorageConnectionString = "";
-        public static string DBFrameworkVersionNumber = "";
+        public static string AzureStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=nunitresults;AccountKey=W1EXYz/WEwTaitt7VK1zqsIZucxNjIikdEdm0kpG7nwZ5wFoHwA/CqkBDdLkg2ZhUJYBBoCf5EgJas0kuYXjDQ==;EndpointSuffix=core.windows.net";
+        public static string DBFrameworkVersionNumber = "3.1.2";
         public static string AzureDevOpsBuildNumber = "LOCAL";
     }
 }
